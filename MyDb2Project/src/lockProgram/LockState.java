6da986f03438b0d6551de539a8954e7838da47a2
mahesh9128 <mahesh9128@gmail.com>
2017-09-17 11:49:01 -0500

    Add files via upload

---
+++ MyDb2Project/src/lockProgram/LockState.java
@@ -0,0 +1,13 @@
+package lockProgram;
+
+/**
+ * Team Members:
+ Manohar, Mahesh: 1001396134
+ Rajakarthikeyan, Bhuvanesh: 1001410051
+ */
+
+//Enum for storing possible types of locks that can be imposed on a data item
+public enum LockState {
+
+    RL, WL, UL
+}
---
+++ MyDb2Project/src/lockProgram/TransactionState.java
@@ -0,0 +1,14 @@
+package lockProgram;
+
+/**
+ * Team Members:
+ Manohar, Mahesh: 1001396134
+ Rajakarthikeyan, Bhuvanesh: 1001410051
+ */
+
+//Enum for storing possible types of states that a transaction can be in.
+public enum TransactionState {
+
+    ACTIVE,BLOCKED ,ABORTED ,COMMITTED;
+
+}
